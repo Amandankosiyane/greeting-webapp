996b093925456457c453e312896ddb0e21c822ab
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-06-08 10:37:54 +0200

    created default route

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-// var express = require('express');
-// var app = express();
-//
-// var people = [];
-// app.get('/greetings/:name', function(req, res) {
-//   var name = req.params.name;
-//   people.push(name);
-//   res.send('Hello,' + name)
-// });
-//
-// app.get('/greeted', function(req, res) {
-//   res.send(people);
-// })
-//
-// app.get('/counter/:name', function(req, res) {
-//   var name = req.params.name;
-//
-//   function counts(input) {
-//     return input == name;
-//   }
-//   var nameCounter = people.filter(counts).length;
-//
-//   res.send("Hello," + name + " " + "has been greeted " + " " +  nameCounter + " time(s).")
-//
-// })
-//
-// var server = app.listen(3000, function() {
-//
-//   var host = server.address().address;
-//   var port = server.address().port;
-//
-//   console.log('Example app listening at http://%s:%s', host, port);
-//
-// });
+const express = require('express');
+// const SubjectRoutes = require('./subjects');
+// const  subjectRoutes = SubjectRoutes();
+const app = express();
+
+app.get('/', function(req,res){
+        res.send('Hellow world');
+})
+// app.get('/subjects', subjectRoutes.index);
+// app.get('/subjects/:add', subjectRoutes.add);
+const port = 3007;
+app.listen(port, function(){
+        console.log('web app started on port : ' + port);
+});
---
+++ subjects.js
@@ -0,0 +1,15 @@
+// module.exports = function() {
+//         const subjectList = [];
+//         const index = function(req, res) {
+//                 res.send(subjectList);
+//         }
+//         const add = function(req, res) {
+//                 var subject = req.params.subject;
+//                 subjectList.push(subject);
+//                 res.redirect('/subjects');
+//         }
+//         return {
+//                 index,
+//                 add
+//         }
+// }
