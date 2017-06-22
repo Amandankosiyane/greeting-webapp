5bce9f1521e74b15b7e388c3b595c608a79934f3
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-06-22 16:02:11 +0200

    installed handlebars and used them as view engine

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+var path = require('path');
 const express = require('express');
-// const SubjectRoutes = require('./subjects');
-// const  subjectRoutes = SubjectRoutes();
+const exphbs = require('express-handlebars');
+const SubjectRoutes = require('./subjects');
+const  subjectRoutes = SubjectRoutes();
 const app = express();
+app.engine('.handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 
 app.get('/', function(req,res){
         res.send('Hellow world');
 })
-// app.get('/subjects', subjectRoutes.index);
-// app.get('/subjects/:add', subjectRoutes.add);
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.get('/subjects', subjectRoutes.index);
+app.get('/subjects/add/:subject', subjectRoutes.add);
 const port = 3007;
 app.listen(port, function(){
         console.log('web app started on port : ' + port);
--- subjects.js
+++ subjects.js
@@ -1,15 +1,27 @@
-// module.exports = function() {
-//         const subjectList = [];
-//         const index = function(req, res) {
-//                 res.send(subjectList);
-//         }
-//         const add = function(req, res) {
-//                 var subject = req.params.subject;
-//                 subjectList.push(subject);
-//                 res.redirect('/subjects');
-//         }
-//         return {
-//                 index,
-//                 add
-//         }
-// }
+module.exports = function() {
+        const subjectList = [];
+
+        const index = function(req, res) {
+                res.render('subjects/index', {subjects : subjectList});
+        }
+
+        const add = function(req, res) {
+                var subject = req.params.subject;
+                var foundSubject = subjectList.find(function(currentSubject) {
+                        return currentSubject === subject
+
+                });
+
+                if (subject && !foundSubject) {
+                        subjectList.push(subject);
+                }
+
+                res.send('Hello, ' + subject + '!');
+                // res.redirect('/subjects');
+        }
+
+        return {
+                index,
+                add
+        }
+}
